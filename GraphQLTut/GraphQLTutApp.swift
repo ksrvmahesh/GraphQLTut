3da8052e2fc6ba20fa0fe3cd3856b656a301d602
mahesh <ksrvmahesh@gmail.com>
2025-01-17 02:11:15 -0500

    Initial Commit

---
+++ GraphQLTut/GraphQLTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLTutApp.swift
+//  GraphQLTut
+//
+//  Created by Mahesh V on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
